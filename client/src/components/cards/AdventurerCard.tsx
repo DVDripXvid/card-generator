49186901133dffe17f74c871c273c2352a6721b8
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2021-03-22 21:55:41 +0100

    display adv. story

--- client/src/components/cards/AdventurerCard.tsx
+++ client/src/components/cards/AdventurerCard.tsx
@@ -11,6 +11,7 @@ const AdventurerCard = forwardRef<HTMLDivElement, IProps>((props, ref) => {
     <div ref={ref} className="flex flex-col gap-1 items-center justify-between w-64 h-96 p-6 bg-yellow-100 text-red-900 font-serif text-xl">
       <h1 className="font-bold text-2xl">{adventurer.name}</h1>
       <h2>{adventurer.cast}</h2>
+      {adventurer.story && <h3 className="italic">{adventurer.story}</h3>}
       <div className="flex flex-row justify-around w-full font-bold">
         <div>{adventurer.strength}</div>
         <div>{adventurer.magic}</div>
