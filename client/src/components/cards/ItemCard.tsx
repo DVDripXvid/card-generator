0b9fbbf9bd6ace1bc6bed8ae1e2c8ef63c90c915
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2021-03-22 20:44:33 +0100

    item bg colors

--- client/src/components/cards/ItemCard.tsx
+++ client/src/components/cards/ItemCard.tsx
@@ -5,10 +5,17 @@ interface IProps {
   item: IItem;
 }
 
+const colorMap: Record<string, string> = {
+  'Gear': 'bg-green-200',
+  'Plot': 'bg-purple-200',
+  'Tale': 'bg-gray-200',
+};
+
 const ItemCard = forwardRef<HTMLDivElement, IProps>((props, ref) => {
   const { item } = props;
+  const bgColor = colorMap[item.cast] || 'bg-pink-200';
   return (
-    <div ref={ref} className="flex flex-col gap-1 items-center justify-around w-64 h-96 p-6 bg-green-200 text-blue-900 font-serif text-xl">
+    <div ref={ref} className={`flex flex-col gap-1 items-center justify-around w-64 h-96 p-6 ${bgColor} text-blue-900 font-serif text-xl`}>
       <h1 className="font-bold text-2xl">{item.name}</h1>
       <h2 className="font-bold">{item.cast}</h2>
       <div>{item.story}</div>
