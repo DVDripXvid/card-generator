c14e509dda041c505cf9173014573ef08772652b
Olivér Rottenhoffer <orottenhoffer@geomant.com>
2021-08-09 22:21:46 +0200

    use lorem pics

--- client/src/components/cards/RoleCard.tsx
+++ client/src/components/cards/RoleCard.tsx
@@ -10,7 +10,7 @@ const RoleCard = forwardRef<HTMLDivElement, IProps>((props, ref) => {
   return (
     <div ref={ref} className="flex flex-col gap-1 items-center justify-between w-64 h-96 p-6 bg-indigo-900 text-gray-200 font-serif text-xl">
       <h1 className="font-bold text-2xl">{role.name}</h1>
-      <img src={`https://randomuser.me/api/portraits/women/${role.id}.jpg`} alt={role.name} />
+      <img src={`https://picsum.photos/seed/${role.name}/200`} alt={role.name} />
       <h3 className="italic">{role.privilege}</h3>
     </div>
   );
